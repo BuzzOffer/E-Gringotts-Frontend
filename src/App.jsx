11dcbb11c5a176c2a499443c378366dc51c2dd2d
Heng Wen Yang <wenyang.heng@outlook.com>
2024-04-18 22:11:53 +0800

    added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Transaction from './pages/Transaction/index.jsx'
+import Payment from './pages/Payment/index.jsx'
+import Home from './pages/Home/index.jsx'
+import Layout from './components/Layout.jsx'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+// client side page routing
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/transaction",
+        element: <Transaction />,
+      },
+      {
+        path: "/payment",
+        element: <Payment />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  }
+]);
 
 function App() {
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
