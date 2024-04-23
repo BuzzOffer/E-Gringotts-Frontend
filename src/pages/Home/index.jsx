bbe2e17e8feba86481d9ad83fdf1dce118b636cc
Simon <hawyee@gmail.com>
2024-04-23 22:01:07 +0800

    Added welcome message in Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,10 @@
+import Welcome from './welcome.jsx'
+
 export default function Home(){
-    return <h1>Home</h1>
+    return (
+        <>
+            <h1>Home</h1>
+            <Welcome />
+        </>
+    );
 }
---
+++ src/pages/Home/welcome.jsx
@@ -0,0 +1,8 @@
+function Welcome() {
+    let user = "Simon Goh"
+    return (
+        <h2>Welcome, {user}</h2>
+    );
+}
+
+export default Welcome
