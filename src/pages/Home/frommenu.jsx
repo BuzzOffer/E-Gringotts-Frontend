7b499efecb094381e5912581f7e7a5cdad40894d
Simon <hawyee@gmail.com>
2024-04-24 17:45:07 +0800

    added frommenu component folder

---
+++ src/pages/Home/frommenu.jsx
@@ -0,0 +1,14 @@
+function FromMenu() {
+    return (
+        <div>
+            <label>From</label>
+            <select>
+                <option>Galleon</option>
+                <option>Sickle</option>
+                <option>Knut</option>
+            </select>
+        </div>
+    );
+}
+
+export default FromMenu
