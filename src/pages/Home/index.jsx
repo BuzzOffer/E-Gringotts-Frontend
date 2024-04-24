40f48b0545b5ba0da601c84e486c45bd497cc19d
Simon <hawyee@gmail.com>
2024-04-24 10:45:28 +0800

    Added balanceSickle component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import Welcome from './welcome.jsx'
 import BalanceGalleon from './balanceGalleon.jsx';
+import BalanceSickle from './balanceSickle.jsx';
 import './home.css'
 
 export default function Home(){
@@ -8,6 +9,7 @@ export default function Home(){
             <h1>Home</h1>
             <Welcome />
             <BalanceGalleon />
+            <BalanceSickle />
         </>
     );
 }
