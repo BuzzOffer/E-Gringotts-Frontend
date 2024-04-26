997ee0b957f54ba3babdf7a846054a95b58e1e0f
Simon <hawyee@gmail.com>
2024-04-26 10:17:05 +0800

    Styled buttons for currency converter

--- src/pages/Home/convertbuttons.jsx
+++ src/pages/Home/convertbuttons.jsx
@@ -1,6 +1,7 @@
 function ConvertButtons() {
     return (
-        <div>
+        <div className="conButtonDiv">
+            <br></br>
             <button id="convertButtons" className="convert">Convert</button>
             <button id="convertButtons" className="confirm">Confirm</button>
         </div>
