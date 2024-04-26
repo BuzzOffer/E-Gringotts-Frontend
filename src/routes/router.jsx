472d0ea91129a900517066b17c629da23ecfd7ad
Heng Wen Yang <wenyang.heng@outlook.com>
2024-04-26 21:01:54 +0800

    payment router modified

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import Transaction from '../pages/Transaction';
-import Payment from '../pages/Payment';
+import { PaymentRoute } from "./payment";
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
@@ -16,19 +16,16 @@ export const router = createBrowserRouter([
             element: <Home />
           },
           {
-            path: "/transaction",
+            path: "transaction",
             element: <Transaction />,
           },
+          PaymentRoute,
           {
-            path: "/payment",
-            element: <Payment />,
-          },
-          {
-            path: "/login",
+            path: "login",
             element: <Login />,
           },
           {
-            path: "/register",
+            path: "register",
             element: <Register />,
           },
         ],
