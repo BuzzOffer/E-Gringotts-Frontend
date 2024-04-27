1ba373e0d8a0e38c5b844782146b5b89187a40b7
Heng Wen Yang <wenyang.heng@outlook.com>
2024-04-27 15:44:11 +0800

    added elements to PaymentReceipt component

--- src/pages/Payment/PaymentReceipt.jsx
+++ src/pages/Payment/PaymentReceipt.jsx
@@ -1,8 +1,45 @@
+import styles from "./PaymentReceipt.module.css";
+
+const transactionDetails = [
+    {
+        label: "Transasction ID",
+        value: "TRX123456"
+    },
+    {
+        label: "Date & Time",
+        value: "11 April 2024, 3:00 PM"
+    },
+    {
+        label: "From",
+        value: "Wen Yang"
+    },
+    {
+        label: "To",
+        value: "Wen Yang"
+    },
+    {
+        label: "Reference",
+        value: "Wen Yang"
+    },
+];
+
 export default function PaymentReceipt(){
     return(
         <>
-            <div>
-                
+            <div className={styles.receiptContainer}>
+                <h1>E-Gringotts Receipt</h1>
+                <div className={styles.statusContainer}>
+                    <p>50.00 Knut</p>
+                    <p>Payment Successfull</p>
+                </div>
+
+                {transactionDetails.map((item) => (
+                    <div className={styles.detailsContainer}>
+                        <p>{item.label}</p>
+                        <p>{item.value}</p>
+                    </div>
+                ))}
+                <button type="button">Continue</button>
             </div>
         </>
     )
