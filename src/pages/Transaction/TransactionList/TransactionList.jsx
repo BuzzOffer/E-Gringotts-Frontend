0c78632d0a794bcf2e9ef66629c8571e580726f0
Heng Wen Yang <wenyang.heng@outlook.com>
2024-05-11 13:53:12 +0800

    add pagination to table

--- src/pages/Transaction/TransactionList/TransactionList.jsx
+++ src/pages/Transaction/TransactionList/TransactionList.jsx
@@ -6,10 +6,11 @@ const BASE_URL = 'http://localhost:8080/api/v1';
 export default function TransactionList(){
     const [error, setError] = useState();
     const [transactions, setTransactions] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const listPerPage = 10;
 
     useEffect(() => {
         const fetchTransactions = async () => {
-            console.log("Fetching data");
             try {
                 const response = await fetch(`${BASE_URL}/transaction/all`);
                 const transactions = await response.json();
@@ -20,36 +21,59 @@ export default function TransactionList(){
         };
 
         fetchTransactions();
-        console.log(transactions);
     }, []);
 
     if(error){
         return <p>Oops..something went wrong while getting transaction data</p>
     }
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Name</th>
-                    <th>Category</th>
-                    <th>Amount</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
+    const startIndex = (currentPage - 1) * listPerPage;
+    const endIndex = startIndex + listPerPage;
+    const currentLists = transactions.slice(startIndex, endIndex);
 
-            <tbody>
-                {transactions.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>Wen Yang</td>
-                        <td>{item.category}</td>
-                        <td>{item.amount}</td>
-                        <td>13 April 2024</td>
+    const handlePrevPage = () => {
+        if(currentPage > 1){
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        const totalPages = Math.ceil(transactions.length / listPerPage);
+        if(currentPage < totalPages){
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Amount</th>
+                        <th>Date</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    {currentLists.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.destinationAccount.myUser.name}</td>
+                            <td>{item.category}</td>
+                            <td>{item.amount}</td>
+                            <td>13 April 2024</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div>
+                <button onClick={handlePrevPage}>Left</button>
+                <button onClick={handleNextPage}>Right</button>
+            </div>
+        </>
     );
 }
