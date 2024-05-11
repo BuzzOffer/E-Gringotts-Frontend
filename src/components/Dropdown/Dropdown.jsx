8dd09063979fd16ed2c3ae8fee078c952d74bb24
Heng Wen Yang <wenyang.heng@outlook.com>
2024-05-11 16:15:14 +0800

    change position property of dropdown and add styling

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import DropdownButton from "./DropdownButton/DropdownButton";
 import DropdownContent from "./DropdownContent/DropdownContent";
+import styles from "./Dropdown.module.css";
 
 export default function Dropdown({ label, options }){
     const [visible, setVisible] = useState(false);
@@ -11,7 +12,7 @@ export default function Dropdown({ label, options }){
     }
 
     return(
-        <div>
+        <div className={styles.dropdownContainer}>
             <DropdownButton toggle={toggleDropdown} label={selectedOption} visible={visible}/>
             <DropdownContent options={options} visible={visible} setSelectedOption={setSelectedOption} toggle={toggleDropdown}/>
         </div>
