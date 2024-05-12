f28ba64890c1eb5c4d5e506507e8905e2324bcc7
Heng Wen Yang <wenyang.heng@outlook.com>
2024-05-12 13:42:42 +0800

    add fetch transaction by category

--- src/pages/Transaction/TransactionList/TransactionList.jsx
+++ src/pages/Transaction/TransactionList/TransactionList.jsx
@@ -1,34 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./TransactionList.module.css";
 import PaginationButton from "./PaginationButton";
 
-const BASE_URL = 'http://localhost:8080/api/v1';
-
-const id = 2;
-
-export default function TransactionList(){
-    const [error, setError] = useState();
-    const [transactions, setTransactions] = useState([]);
+export default function TransactionList({ transactions, id }){
     const [currentPage, setCurrentPage] = useState(1);
     const listPerPage = 10;
-
-    useEffect(() => {
-        const fetchTransactions = async () => {
-            try {
-                const response = await fetch(`${BASE_URL}/transaction/all?id=${id}`);
-                const transactions = await response.json();
-                setTransactions(transactions);
-            } catch (e) {
-                setError(e);
-            } 
-        };
-
-        fetchTransactions();
-    }, []);
-
-    if(error){
-        return <p>Oops..something went wrong while getting transaction data</p>
-    }
 
     // the component renders again whenever setCurrentPage is called, which means the startIndex and endIndex will change
     const startIndex = (currentPage - 1) * listPerPage;
--- src/pages/Transaction/index.jsx
+++ src/pages/Transaction/index.jsx
@@ -1,23 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import styles from "./Transaction.module.css";
 import TransactionList from "./TransactionList/TransactionList";
 
 const lastNDays = [30, 15, 10];
-const categories = ["Entertainment", "Food", "Misc"]
+const categories = ["Entertainment", "Food", "Misc.", "Game"]
+const BASE_URL = 'http://localhost:8080/api/v1';
+const id = 2;
 
 export default function Transaction(){
 
     const [selectedDays, setSelectedDays] = useState("Last 30 days");
     const [selectedCateory, setSelectedCategory] = useState("Category");
+    const [error, setError] = useState();
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        const fetchTransactions = async () => {
+            try {
+                const response = await fetch(`${BASE_URL}/transaction/all?id=${id}`);
+                const transactions = await response.json();
+                setTransactions(transactions);
+            } catch (e) {
+                setError(e);
+            } 
+        };
+
+        fetchTransactions();
+    }, []);
 
     const handleDaysChange = (item) =>{
         setSelectedDays(`Last ${item} days`);
     };
 
-    const handleCateogoryChange = (item) => {
+    const handleCateogoryChange = async (item) => {
+        try {
+            const response = await fetch(`${BASE_URL}/transaction/getTransaction?id=${id}&property=category&value=${item}`);
+            const transactions = await response.json();
+            setTransactions(transactions);
+        } catch (error) {
+            setError(error);
+        }
         setSelectedCategory(item);
     };
+
+    if(error){
+        return <p>Oops..something went wrong while getting transaction data</p>
+    }
     
     return (
         <>
@@ -26,7 +55,7 @@ export default function Transaction(){
                 <Dropdown label={selectedDays} options={lastNDays} onOptionClicked={handleDaysChange}/>
                 <Dropdown label={selectedCateory} options={categories} onOptionClicked={handleCateogoryChange}/>
             </div>
-            <TransactionList />
+            <TransactionList transactions={transactions} id={id}/>
         </>
     )
 }
