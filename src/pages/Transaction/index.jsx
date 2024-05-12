b86b8318a6d6f36efd053cccbe919c351433b42f
Heng Wen Yang <wenyang.heng@outlook.com>
2024-05-12 15:33:15 +0800

    added gaps to filter buttons and clear filter btn

--- src/pages/Transaction/index.jsx
+++ src/pages/Transaction/index.jsx
@@ -5,6 +5,7 @@ import TransactionList from "./TransactionList/TransactionList";
 
 const lastNDays = [30, 15, 10];
 const categories = ["Entertainment", "Food", "Misc.", "Game"]
+const types = ["Sent", "Receive", "Convert"];
 const BASE_URL = 'http://localhost:8080/api/v1';
 const id = 2;
 
@@ -12,8 +13,10 @@ export default function Transaction(){
 
     const [selectedDays, setSelectedDays] = useState("Last 30 days");
     const [selectedCateory, setSelectedCategory] = useState("Category");
+    const [selectedType, setSelectedType] = useState("Type");
     const [error, setError] = useState();
     const [transactions, setTransactions] = useState([]);
+    const [filteredTransactions, setFilteredTransactions] = useState([]);
 
     useEffect(() => {
         const fetchTransactions = async () => {
@@ -21,6 +24,7 @@ export default function Transaction(){
                 const response = await fetch(`${BASE_URL}/transaction/all?id=${id}`);
                 const transactions = await response.json();
                 setTransactions(transactions);
+                setFilteredTransactions(transactions);
             } catch (e) {
                 setError(e);
             } 
@@ -37,12 +41,42 @@ export default function Transaction(){
         try {
             const response = await fetch(`${BASE_URL}/transaction/getTransaction?id=${id}&property=category&value=${item}`);
             const transactions = await response.json();
-            setTransactions(transactions);
+            setFilteredTransactions(transactions);
         } catch (error) {
             setError(error);
         }
         setSelectedCategory(item);
     };
+
+    const handleTypeChange = (item) => {
+        setSelectedType(item);
+
+        let filteredTransactions;
+        switch(item){
+            case "Sent":
+                filteredTransactions = transactions.filter(tx => tx.source_account_id_long === id);
+                break;
+            case "Receive":
+                filteredTransactions = transactions.filter(tx => tx.destination_account_id_long === id);
+                break;
+            case "Convert":
+                filteredTransactions = transactions.filter(tx => tx.destination_account_id_long === tx.source_account_id_long);
+                break;
+            default:
+                filteredTransactions = transactions;
+        }
+
+        setFilteredTransactions(filteredTransactions);
+    }
+
+    const clearFilters = () => {
+        setSelectedDays("Last 30 days");
+        setSelectedCategory("Category");
+        setSelectedType("Type");
+        setFilteredTransactions(transactions);
+        console.log("filter cleared");
+        console.log(transactions);
+    }
 
     if(error){
         return <p>Oops..something went wrong while getting transaction data</p>
@@ -54,8 +88,10 @@ export default function Transaction(){
             <div className={styles.filterContainer}>
                 <Dropdown label={selectedDays} options={lastNDays} onOptionClicked={handleDaysChange}/>
                 <Dropdown label={selectedCateory} options={categories} onOptionClicked={handleCateogoryChange}/>
+                <Dropdown label={selectedType} options={types} onOptionClicked={handleTypeChange}/>
             </div>
-            <TransactionList transactions={transactions} id={id}/>
+            <button onClick={clearFilters}>Clear Filter</button>
+            <TransactionList transactions={filteredTransactions} id={id}/>
         </>
     )
 }
