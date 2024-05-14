aac16ab30048cb73d82ba0b84eca9da3710a172a
Simon <hawyee@gmail.com>
2024-05-14 22:14:47 +0800

    Added condition to check for max value of amount

--- src/pages/Home/converter.js
+++ src/pages/Home/converter.js
@@ -10,38 +10,44 @@ export function convert() {
     toCurrency = document.querySelector("#toCurrency").value;
     amount = document.getElementById("amount").value;
     convertedAmount = amount;
-    switch (fromCurrency) {
-        case "Galleon":
-            switch (toCurrency) {
-                case "Sickle":
-                    convertedAmount = amount * 17;
-                    break;
-                case "Knut":
-                    convertedAmount = amount * 493;
-                    break;
-            }
-            break;
-        case "Sickle":
-            switch(toCurrency) {
-                case "Galleon":
-                    convertedAmount = amount * 0.05882;
-                    break;
-                case "Knut":
-                    convertedAmount = amount * 29;
-                    break;
-            }
-            break;
-        case "Knut":
-            switch(toCurrency) {
-                case "Galleon":
-                    convertedAmount = amount * 0.002028;
-                    break;
-                case "Sickle":
-                    convertedAmount = amount * 0.03448;
-                    break;
-            }
-            break;
+    if (amount > 100000) {
+        fromValue.textContent = `Amount is too large! Max 100000`;
+        toValue.textContent = "";
+    } 
+    else {
+        switch (fromCurrency) {
+            case "Galleon":
+                switch (toCurrency) {
+                    case "Sickle":
+                        convertedAmount = amount * 17;
+                        break;
+                    case "Knut":
+                        convertedAmount = amount * 493;
+                        break;
+                }
+                break;
+            case "Sickle":
+                switch(toCurrency) {
+                    case "Galleon":
+                        convertedAmount = amount * 0.05882;
+                        break;
+                    case "Knut":
+                        convertedAmount = amount * 29;
+                        break;
+                }
+                break;
+            case "Knut":
+                switch(toCurrency) {
+                    case "Galleon":
+                        convertedAmount = amount * 0.002028;
+                        break;
+                    case "Sickle":
+                        convertedAmount = amount * 0.03448;
+                        break;
+                }
+                break;
+        }
+        fromValue.textContent = `${amount} ${fromCurrency} = `;
+        toValue.textContent = `${convertedAmount.toFixed(2)} ${toCurrency}`;
     }
-    fromValue.textContent = `${amount} ${fromCurrency} = `;
-    toValue.textContent = `${convertedAmount.toFixed(2)} ${toCurrency}`;
 }
