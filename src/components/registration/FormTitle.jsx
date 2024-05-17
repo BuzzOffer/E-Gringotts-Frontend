0ead36a8059eb488de0586c6d5d3d1897c9e44a6
Jonathan Siew <jonathansiew.2004@gmail.com>
2024-05-17 17:13:29 +0800

    Update FormTitle component to support text alignment options

--- src/components/registration/FormTitle.jsx
+++ src/components/registration/FormTitle.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import './FormTitle.css'; // Importing CSS specific to FormTitle
 
-function FormTitle({ text }) {
+function FormTitle({ text, align = 'center'}) {
+  const cssClass = align === 'center' ? 'form-title-center' : align === 'left' ? 'form-title-left' : 'form-title-right';
   return (
     <div>
-    <h2 className="form-title">{text}</h2>
+    <h2 className={cssClass}>{text}</h2>
     </div>
   );
 }
