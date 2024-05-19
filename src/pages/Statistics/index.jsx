2e5d505743f472e10a0d80435c2d8ec60f1ff930
Simon <hawyee@gmail.com>
2024-05-19 19:12:43 +0800

    Added divs and linegraph component

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -1,7 +1,16 @@
+import { Line, Pie, Bar } from "react-chartjs-2";
+
 export default function Statistics() {
     return (
         <>
             <h1>Statistics</h1>
+            <div id="visuals">
+                <div id="lineGraph">
+                    <Line></Line>
+                </div>
+                <div id="pieChart"></div>
+                <div id="barChart"></div>
+            </div>
         </>
     );
 }
