a25c6cc91983666b7a1279f288f314e25942a372
Simon <hawyee@gmail.com>
2024-05-21 17:05:13 +0800

    Added function to back button

--- src/pages/Statistics/backbutton.jsx
+++ src/pages/Statistics/backbutton.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
+
 function BackButton() {
+    const navigate = useNavigate();
+
     return (
         <div>
-            <button className="back-button">← Back</button>
+            <button className="back-button" onClick={() => navigate(-1)}>← Back</button>
         </div>
     )
 }
