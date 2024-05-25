699ea3490214ab1e77245c771304ae28c119ff27
Simon <hawyee@gmail.com>
2024-05-25 14:00:52 +0800

    Modified code to fetch API

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -1,7 +1,6 @@
 import { Chart as ChartJS, defaults} from 'chart.js/auto'
 import { Line, Pie, Bar } from "react-chartjs-2";
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import './stats.css';
 import expensesData from './data/expensesData.json';
 import GraphSelect from './graphselect';
@@ -11,6 +10,35 @@ defaults.maintainAspectRatio = false;
 defaults.responsive = true;
 
 export default function Statistics() {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch(`http://localhost:8080/api/v1/transaction/getTransaction?property=source_account&value=23`)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setData({
+                    labels: data.map((item) => item.category),
+                    datasets: [
+                        {
+                            label: "Funds",
+                            data: data.map((item) => item.amount),
+                            backgroundColor: [
+                                "#7e0af2",
+                                "#0af2e6",
+                                "#93db0d",
+                                "#ff5500"
+                            ]
+                        }
+                    ]
+                })
+            })
+            
+        }
+        fetchData();
+    })
 
     const daily = {
         labels: expensesData.map((data) => data.label),
@@ -57,29 +85,7 @@ export default function Statistics() {
             },
         ]
     }
-    const [data, setData] = useState({});
-    useEffect(() => {
-        const fetchData = async () => {
-            const { data } = await axios.get(`http://localhost:8080/api/v1/transaction`)
-            console.log(data);
-            setData({
-                labels: data.data.map((item) => item.category),
-                datasets: [
-                    {
-                        label: "Galleon",
-                        data: data.data.map((item) => item.amount),
-                        backgroundColor: [
-                            "#7e0af2",
-                            "#0af2e6",
-                            "#93db0d",
-                            "#ff5500"
-                        ]
-                    }
-                ]
-            })
-        }
-        fetchData();
-    })
+    
     const updateGraph = (event) => {
         setData(event.target.value);
     }
