6c01cfb8d3c102bb18115e16886db1c235ba64f5
Simon <hawyee@gmail.com>
2024-05-25 17:22:42 +0800

    Successfully fetched data from API

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -10,7 +10,58 @@ defaults.maintainAspectRatio = false;
 defaults.responsive = true;
 
 export default function Statistics() {
-    const [data, setData] = useState({});
+
+    const daily = {
+        labels: expensesData.map((data) => data.label),
+        datasets: [
+            {
+                label: "Galleon",
+                data: expensesData.map((data) => data.value),
+                backgroundColor: [
+                "#7e0af2",
+                "#0af2e6",
+                "#93db0d",
+                "#ff5500"
+                ]
+            },
+        ]
+    }
+    // const monthly = {
+    //     labels: expensesData.map((data) => data.label),
+    //     datasets: [
+    //         {
+    //             label: "Sickle",
+    //             data: expensesData.map((data) => data.value),
+    //             backgroundColor: [
+    //             "#7e0af2",
+    //             "#0af2e6",
+    //             "#93db0d",
+    //             "#ff5500"
+    //             ]
+    //         },
+    //     ]
+    // }
+    // const allTime = {
+    //     labels: expensesData.map((data) => data.label),
+    //     datasets: [
+    //         {
+    //             label: "Knut",
+    //             data: expensesData.map((data) => data.value),
+    //             backgroundColor: [
+    //             "#7e0af2",
+    //             "#0af2e6",
+    //             "#93db0d",
+    //             "#ff5500"
+    //             ]
+    //         },
+    //     ]
+    // }
+    
+    // const updateGraph = (event) => {
+    //     setChartData(event.target.value);
+    // }
+
+    const [chartData, setChartData] = useState(daily);
     useEffect(() => {
         const fetchData = async () => {
             fetch(`http://localhost:8080/api/v1/transaction/getTransaction?property=source_account&value=23`)
@@ -19,7 +70,7 @@ export default function Statistics() {
             })
             .then(data => {
                 console.log(data);
-                setData({
+                setChartData({
                     labels: data.map((item) => item.category),
                     datasets: [
                         {
@@ -40,70 +91,20 @@ export default function Statistics() {
         fetchData();
     })
 
-    const daily = {
-        labels: expensesData.map((data) => data.label),
-        datasets: [
-            {
-                label: "Galleon",
-                data: expensesData.map((data) => data.value),
-                backgroundColor: [
-                "#7e0af2",
-                "#0af2e6",
-                "#93db0d",
-                "#ff5500"
-                ]
-            },
-        ]
-    }
-    const monthly = {
-        labels: expensesData.map((data) => data.label),
-        datasets: [
-            {
-                label: "Sickle",
-                data: expensesData.map((data) => data.value),
-                backgroundColor: [
-                "#7e0af2",
-                "#0af2e6",
-                "#93db0d",
-                "#ff5500"
-                ]
-            },
-        ]
-    }
-    const allTime = {
-        labels: expensesData.map((data) => data.label),
-        datasets: [
-            {
-                label: "Knut",
-                data: expensesData.map((data) => data.value),
-                backgroundColor: [
-                "#7e0af2",
-                "#0af2e6",
-                "#93db0d",
-                "#ff5500"
-                ]
-            },
-        ]
-    }
-    
-    const updateGraph = (event) => {
-        setData(event.target.value);
-    }
-
     return (
         <>
             <h1>Statistics</h1>
             <BackButton />
-            <GraphSelect onOption={updateGraph}/>
+            <GraphSelect/>
             <div className="visuals">
                 <div className="pieChart">
                     <Pie 
-                        data={data}
+                        data={chartData}
                     />
                 </div>
                 <div className="barChart">
                     <Bar 
-                        data={data}
+                        data={chartData}
                     />
                 </div>
             </div>
